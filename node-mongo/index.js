9cbdb69a54e6937fe64750652fec5ffbe6a866fb
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-26 01:49:22 +0600

    node first module done

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,9 +1,34 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser')
 
+const app = express();
+app.use(cors());
+app.use(bodyParser.json())
+
+
+
+const users = ['asad', 'amin', 'alamin', 'nuru', 'shahded', 'lira'];
 
 app.get('/', (req, res) => {
     res.send('hello from node...!!!');
 })
+
+app.get('/fruits', (req, res) => {
+    res.send({ fruits: 'Banana', Quantity: 100, price: 1000 });
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id;
+    const name = users[id];
+    res.send({ id, name });
+})
+
+// post req  
+app.post('/addUser', (req, res) => {
+    const user = req.body;
+    res.send(user);
+    //console.log(req.body)
+});
 
 app.listen(3000, () => console.log('app is running'));
