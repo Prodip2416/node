85b0f30b1e95ac2067c379e9f3d54d40aa4c16d0
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-25 23:19:13 +0600

    first commit

---
+++ node-mongo/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('hello from node...');
+})
+
+app.listen(3000, () => console.log('app is running'));
