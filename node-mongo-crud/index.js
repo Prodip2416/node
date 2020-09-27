bd26b7fbebeabd8ff8df49405bfc386e4f2dd3aa
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-28 00:34:36 +0600

    CRUD with node

--- node-mongo-crud/index.js
+++ node-mongo-crud/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+const ObjectID = require('mongodb').ObjectID;
 const password = 'fTY5sXUsxlELvGRC';
 const dbName = 'organicdb';
 
@@ -21,14 +22,51 @@ client.connect(function (err) {
     if (err) console.log('Err::', err);
     const collection = client.db(dbName).collection('products');
 
+    // Get all Product
+    app.get('/products', (req, res) => {
+        collection.find({}) //.limit(2)
+            .toArray((err, result) => {
+                res.send(result);
+            });
+    });
+
+    // get a single item
+    app.get('/product/:id', (req, res) => {
+        collection.find({ _id: ObjectID(req.params.id) }) //.limit(2)
+            .toArray((err, result) => {
+                res.send(result[0]);
+            });
+    });
+
+    // Add Product to database
     app.post('/addProduct', (req, res) => {
         const product = req.body;
         collection.insertOne(product)
             .then(result => {
-                console.log('Inserted Successfully');
-                res.send('Successfully added');
+                // console.log('Inserted Successfully');
+                // res.send('item added successfully');
+                res.redirect('/');
             })
     })
+
+    // Delete a product
+    app.delete('/delete/:id', (req, res) => {
+        collection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            })
+    })
+
+    // Update single item
+    app.patch('/update/:id', (req, res) => {
+        collection.updateOne({ _id: ObjectID(req.params.id) },
+            {
+                $set: { price: req.body.price, quantity: req.body.quantity }
+            })
+            .then(result => {
+                res.send(result.modifiedCount > 0);
+            })
+    });
 
     // client.close();
 });
