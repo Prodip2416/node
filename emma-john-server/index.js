682243119a63f51b78ea75442c6cb125e98e86d1
prodip2416 <prodip.sarker.cse@gmail.com>
2020-10-02 01:38:57 +0600

    load data from database

--- emma-john-server/index.js
+++ emma-john-server/index.js
@@ -14,12 +14,29 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 
 client.connect(err => {
     const productCollection = client.db(process.env.DB_NAME).collection(process.env.DB_COLLECTION);
-
+    // Delete all documents form the collection
+    //productCollection.deleteMany({}).then(res=>{console.log('deleted')})
+    
     app.post('/addProduct', (req, res) => {
         const product = req.body;
+        //console.log(product);
         productCollection.insertMany(product)
             .then(result => {
-                console.log(result.insertedCount);
+                res.send(result.insertedCount);
+            })
+    });
+
+    app.get('/products', (req, res) => {
+        productCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    });
+
+    app.get('/products/:key', (req, res) => {
+        productCollection.find({key: req.params.key})
+            .toArray((err, documents) => {
+                res.send(documents[0]);
             })
     });
 });
